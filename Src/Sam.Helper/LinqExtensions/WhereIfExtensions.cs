0da1a3d1482677960b4808e7f2f465297095ec50
saman azadi <samanazadi1996@gmail.com>
2023-10-28 21:18:16 +0330

    Add project files.

---
+++ Src/Sam.Helper/LinqExtensions/WhereIfExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Sam.Helper.LinqExtensions
+{
+    public static class WhereIfExtensions
+    {
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
+        {
+            if (condition)
+            {
+                return query.Where(predicate);
+            }
+            return query;
+        }
+
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> collection, bool condition, Func<T, bool> predicate)
+        {
+            if (condition)
+            {
+                return collection.Where(predicate);
+            }
+            return collection;
+        }
+
+        public static ICollection<T> WhereIf<T>(this ICollection<T> collection, bool condition, Func<T, bool> predicate)
+        {
+            if (condition)
+            {
+                return collection.Where(predicate).ToList();
+            }
+            return collection;
+        }
+
+        public static List<T> WhereIf<T>(this List<T> list, bool condition, Func<T, bool> predicate)
+        {
+            if (condition)
+            {
+                return list.Where(predicate).ToList();
+            }
+            return list;
+        }
+
+        public static T[] WhereIf<T>(this T[] array, bool condition, Predicate<T> predicate)
+        {
+            if (condition)
+            {
+                return Array.FindAll(array, predicate);
+            }
+            return array;
+        }
+
+        public static HashSet<T> WhereIf<T>(this HashSet<T> set, bool condition, Func<T, bool> predicate)
+        {
+            if (condition)
+            {
+                var resultSet = new HashSet<T>();
+                foreach (var item in set)
+                {
+                    if (predicate(item))
+                    {
+                        resultSet.Add(item);
+                    }
+                }
+                return resultSet;
+            }
+            return set;
+        }
+
+    }
+}
---
+++ Tests/Sam.Helper.UnitTests/LinqExtensionsTests/WhereIfExtensionsTests.cs
@@ -0,0 +1,128 @@
+﻿using System.Linq;
+using Sam.Helper.LinqExtensions;
+
+namespace Sam.Helper.UnitTests.LinqExtensionsTests
+{
+    public class WhereIfExtensionsTests
+    {
+        #region IQueryable Tests
+
+        [Fact]
+        public void WhereIf_Should_Filter_IQueryable_When_Condition_True()
+        {
+            IQueryable<int> query = new List<int> { 1, 2, 3, 4, 5 }.AsQueryable();
+            var filteredQuery = query.WhereIf(true, x => x > 3);
+            Assert.Equal(2, filteredQuery.Count());
+        }
+
+        [Fact]
+        public void WhereIf_Should_Not_Filter_IQueryable_When_Condition_False()
+        {
+            IQueryable<int> query = new List<int> { 1, 2, 3, 4, 5 }.AsQueryable();
+            var filteredQuery = query.WhereIf(false, x => x > 3);
+            Assert.Equal(5, filteredQuery.Count());
+        }
+
+        #endregion
+
+        #region ICollection Tests
+
+        [Fact]
+        public void WhereIf_Should_Filter_ICollection_When_Condition_True()
+        {
+            ICollection<int> collection = new List<int> { 1, 2, 3, 4, 5 };
+            var filteredCollection = collection.WhereIf(true, x => x > 3);
+            Assert.Equal(2, filteredCollection.Count);
+        }
+
+        [Fact]
+        public void WhereIf_Should_Not_Filter_ICollection_When_Condition_False()
+        {
+            ICollection<int> collection = new List<int> { 1, 2, 3, 4, 5 };
+            var filteredCollection = collection.WhereIf(false, x => x > 3);
+            Assert.Equal(5, filteredCollection.Count);
+        }
+
+        #endregion
+
+        #region HashSet Tests
+
+        [Fact]
+        public void WhereIf_Should_Filter_HashSet_When_Condition_True()
+        {
+            HashSet<int> set = new HashSet<int> { 1, 2, 3, 4, 5 };
+            var filteredSet = set.WhereIf(true, x => x > 3);
+            Assert.Equal(2, filteredSet.Count);
+        }
+
+        [Fact]
+        public void WhereIf_Should_Not_Filter_HashSet_When_Condition_False()
+        {
+            HashSet<int> set = new HashSet<int> { 1, 2, 3, 4, 5 };
+            var filteredSet = set.WhereIf(false, x => x > 3);
+            Assert.Equal(5, filteredSet.Count);
+        }
+
+        #endregion
+
+        #region Enumerable Tests
+
+        [Fact]
+        public void WhereIf_Should_Filter_Enumerable_When_Condition_True()
+        {
+            IEnumerable<int> collection = new List<int> { 1, 2, 3, 4, 5 };
+            var filteredCollection = collection.WhereIf(true, x => x > 3);
+            Assert.Equal(2, filteredCollection.Count());
+        }
+
+        [Fact]
+        public void WhereIf_Should_Not_Filter_Enumerable_When_Condition_False()
+        {
+            IEnumerable<int> collection = new List<int> { 1, 2, 3, 4, 5 };
+            var filteredCollection = collection.WhereIf(false, x => x > 3);
+            Assert.Equal(5, filteredCollection.Count());
+        }
+
+        #endregion
+
+        #region List Tests
+
+        [Fact]
+        public void WhereIf_Should_Filter_List_When_Condition_True()
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+            var filteredList = list.WhereIf(true, x => x > 3);
+            Assert.Equal(2, filteredList.Count);
+        }
+
+        [Fact]
+        public void WhereIf_Should_Not_Filter_List_When_Condition_False()
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+            var filteredList = list.WhereIf(false, x => x > 3);
+            Assert.Equal(5, filteredList.Count);
+        }
+
+        #endregion
+
+        #region Array Tests
+
+        [Fact]
+        public void WhereIf_Should_Filter_Array_When_Condition_True()
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            var filteredArray = array.WhereIf(true, x => x > 3);
+            Assert.Equal(2, filteredArray.Length);
+        }
+
+        [Fact]
+        public void WhereIf_Should_Not_Filter_Array_When_Condition_False()
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            var filteredArray = array.WhereIf(false, x => x > 3);
+            Assert.Equal(5, filteredArray.Length);
+        }
+
+        #endregion
+    }
+}
